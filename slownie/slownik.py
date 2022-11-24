1921d9b4c7908e72865786d501b5aade8f36c20e
Tungsten03 <104135139+Tungsten03@users.noreply.github.com>
2022-11-24 20:22:27 +0100

    krotki

--- slownie/slownik.py
+++ slownie/slownik.py
@@ -1,48 +1,52 @@
-setki = {
-    9: 'dziewiecset',
-    8: 'osiemset',
-    7: 'siedemset',
-    6: 'szescset',
-    5: 'piecset',
-    4: 'czterysta',
-    3: 'trzysta',
-    2: 'dwiescie',
-    1: 'sto'
-}
+setki = (
+    '',
+    'sto',
+    'dwiescie',
+    'trzysta',
+    'czterysta',
+    'piecset',
+    'szescset',
+    'siedemset',
+    'osiemset',
+    'dziewiecset'
+)
 
-dziesiatki = {
-    9: 'dziewiecdziesiat',
-    8: 'osiemdziesiat',
-    7: 'siedemdziesiat',
-    6: 'szescdziesiat',
-    5: 'piecdziesiat',
-    4: 'czterdziesci',
-    3: 'trzydziesci',
-    2: 'dwadziescia',
-    1: 'dziesiec'
-}
+dziesiatki = (
+    '',
+    'dziesiec',
+    'dwadziescia',
+    'trzydziesci',
+    'czterdziesci',
+    'piecdziesiat',
+    'szescdziesiat',
+    'siedemdziesiat',
+    'osiemdziesiat',
+    'dziewiecdziesiat'
+)
 
-nastki = {
-    9: 'dziewietnascie',
-    8: 'osiemnascie',
-    7: 'siedemnascie',
-    6: 'szesnascie',
-    5: 'pietnascie',
-    4: 'czternaście',
-    3: 'trzynaście',
-    2: 'dwanaście',
-    1: 'jedenaście',
-}
+nastki = (
+    '',
+    'jedenascie',
+    'dwanascie',
+    'trzynascie',
+    'czternascie',
+    'pietnascie',
+    'szesnascie',
+    'siedemnascie',
+    'osiemnascie',
+    'dziewietnascie'
+)
 
-jednosci = {
-    9: 'dziewiec',
-    8: 'osiem',
-    7: 'siedem',
-    6: 'szesc',
-    5: 'piec',
-    4: 'cztery',
-    3: 'trzy',
-    2: 'dwa',
-    1: 'jeden',
-    0: 'zero'
-}
+
+jednosci = (
+    'zero',
+    'jeden',
+    'dwa',
+    'trzy',
+    'cztery',
+    'piec',
+    'szesc',
+    'siedem',
+    'osiem',
+    'dziewiec'
+)
