241a8cda95fc3012a7a6b226a3dbe3237eff6aa7
Tungsten03 <104135139+Tungsten03@users.noreply.github.com>
2022-11-24 20:21:47 +0100

    wersja 2 po uwagach

--- slownie/liczba_slownie.py
+++ slownie/liczba_slownie.py
@@ -3,26 +3,28 @@ from slownik import setki, dziesiatki, nastki, jednosci
 liczba = input('podaj liczbe w zakresie 0-999: ')
 lista = [int(i) for i in liczba]
 zakres = len(lista)
+slownie = ''
 
-match zakres:
-    case 1:
-        print(jednosci[lista[0]])
-    case 2:
-        if lista[0] == 1:
-            print(nastki[lista[1]])
+if zakres == 3:
+    slownie += setki[lista[0]]
+    if lista[1] == 1:
+        slownie += nastki[lista[2]]
+    else:
+        slownie += dziesiatki[lista[1]]
+        if lista[2] == 0:
+            pass
         else:
-            if lista[1] == 0:
-                print(dziesiatki[lista[0]])
-            else:
-                print(dziesiatki[lista[0]] + jednosci[lista[0]])
-    case 3:
-        if lista[1] == 0 and lista[2] == 0:
-            print(setki[lista[0]])
-        elif lista[2] != 0 and lista[1] == 1:
-            print(setki[lista[0]] + nastki[lista[2]])
-        elif lista[2] == 0 and lista[1] != 1:
-            print(setki[lista[0]] + dziesiatki[lista[1]])
-        elif lista[2] != 0 and lista[1] == 0:
-            print(setki[lista[0]] + jednosci[lista[2]])
+            slownie += jednosci[lista[2]]
+elif zakres == 2:
+    if lista[0] == 1:
+        slownie += nastki[lista[1]]
+    else:
+        slownie += dziesiatki[lista[0]]
+        if lista[1] == 0:
+            pass
         else:
-            print(setki[lista[0]] + dziesiatki[lista[1]] + jednosci[lista[2]])
+            slownie += jednosci[lista[1]]
+else:
+    slownie += jednosci[lista[0]]
+
+print(slownie)
